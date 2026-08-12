c22dac1af64c756a62338364ba83a160cd1c4781
Guilherme Amos <guilherme_amos@hotmail.com>
2026-08-12 13:37:32 -0300

    Desafio concluído

--- Program.cs
+++ Program.cs
@@ -1,27 +1,44 @@
 using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
 using TrilhaApiDesafio.Context;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-builder.Services.AddDbContext<OrganizadorContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+builder.Services.AddAuthorization(); 
 
-builder.Services.AddControllers().AddJsonOptions(options =>
-    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+builder.Services.AddControllers();
+builder.Services.AddOpenApi();
+
+// 1. Busque a string de conexão do seu appsettings.json
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// 2. Configure o DbContext usando o nome CORRETO da classe: OrganizadorContext
+// builder.Services.AddDbContext<OrganizadorContext>(options => 
+//     options.UseMySql(connectionString, new MySqlServerVersion(new Version(8, 0, 30))));
+
+// Antes (Pomelo): builder.Services.AddDbContext<Context>(options => options.UseMySql(...));
+
+// Agora (Oficial MySQL):
+// builder.Services.AddDbContext<OrganizadorContext>(options => 
+//     options.UseMySQL(builder.Configuration.GetConnectionString("ConexaoPadrao")));
+
+// Cole esta linha provisória no lugar:
+builder.Services.AddDbContext<OrganizadorContext>(options => 
+    options.UseInMemoryDatabase("DesafioDio"));
+
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.MapOpenApi();
+    app.MapScalarApiReference();
 }
 
 app.UseHttpsRedirection();
@@ -31,3 +48,7 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+internal class TarefaContext
+{
+}
